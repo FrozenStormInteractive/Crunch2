cb8b24f70b7e9be8c4a06c8badf67e5f2a0bee65
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-30 23:15:49 -0400

    format executable sources

--- crunch/corpus_test.h
+++ crunch/corpus_test.h
@@ -4,23 +4,24 @@
 #include "crn_command_line_params.h"
 #include "crn_image_utils.h"
 
-namespace crnlib {
-class corpus_tester {
- public:
-  corpus_tester();
+namespace crnlib
+{
+    class corpus_tester
+    {
+    public:
+        corpus_tester();
 
-  bool test(const char* pCmd_line);
+        bool test(const char* pCmd_line);
 
- private:
-  void print_comparative_metric_stats(const command_line_params& params, const crnlib::vector<image_utils::error_metrics>& stats1, const crnlib::vector<image_utils::error_metrics>& stats2, uint num_blocks_x, uint num_blocks_y);
-  void print_metric_stats(const crnlib::vector<image_utils::error_metrics>& stats, uint num_blocks_x, uint num_blocks_y);
+    private:
+        void print_comparative_metric_stats(const command_line_params& params, const crnlib::vector<image_utils::error_metrics>& stats1, const crnlib::vector<image_utils::error_metrics>& stats2, uint num_blocks_x, uint num_blocks_y);
+        void print_metric_stats(const crnlib::vector<image_utils::error_metrics>& stats, uint num_blocks_x, uint num_blocks_y);
 
-  image_u8 m_bad_block_img;
-  uint m_next_bad_block_index;
-  uint m_total_bad_block_files;
+        image_u8 m_bad_block_img;
+        uint m_next_bad_block_index;
+        uint m_total_bad_block_files;
 
-  void flush_bad_blocks();
-  void add_bad_block(image_u8& block);
-};
-
-}  // namespace crnlib
+        void flush_bad_blocks();
+        void add_bad_block(image_u8& block);
+    };
+} // namespace crnlib
