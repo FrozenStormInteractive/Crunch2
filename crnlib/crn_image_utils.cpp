b42ee7fc59ff829de08931d575fbb37212bc6512
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-31 18:15:20 -0400

    Use static stb

--- crnlib/crn_image_utils.cpp
+++ crnlib/crn_image_utils.cpp
@@ -5,8 +5,10 @@
 #include <jpgd.h>
 #include <miniz.h>
 
+#define STB_IMAGE_STATIC
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#define STB_IMAGE_WRITE_STATIC
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
--- examples/example3/example3.cpp
+++ examples/example3/example3.cpp
@@ -23,7 +23,9 @@
 #pragma warning(disable : 4127)  // conditional expression is constant
 #endif
 
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 using namespace crnlib;
