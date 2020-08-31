b66c417e5502ad42bc0e666402fbda7dfb2a1218
Yoann Potinet <yoann.potinet@gmail.com>
2020-08-31 18:47:20 -0400

    idk what im doing

--- crnlib/crn_mem.cpp
+++ crnlib/crn_mem.cpp
@@ -16,7 +16,18 @@
 #include <malloc.h>
 #define _msize malloc_usable_size
 #elif defined(CRN_OS_DARWIN)
-#include <uuid/uuid.h>
+
+struct uuid_t {
+    unsigned long        time_low;
+    unsigned short        time_mid;
+    unsigned short        time_hi_and_version;
+    unsigned char        clock_seq_hi_and_reserved;
+    unsigned char        clock_seq_low;
+    unsigned char        node[6];
+};
+
+typedef struct uuid_t uuid_t;
+
 #include <malloc/malloc.h>
 #define _msize malloc_size
 #endif
