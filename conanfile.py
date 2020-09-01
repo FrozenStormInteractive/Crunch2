01bb0ab6ca0d4346660693109194b32da12e6dfb
Yoann Potinet <yoann.potinet@gmail.com>
2020-09-01 15:03:19 -0400

    Add conan support

---
+++ conanfile.py
@@ -0,0 +1,60 @@
+import os
+from conans import ConanFile, CMake, tools
+
+class CrunchConan(ConanFile):
+    name = "crunch"
+    description = "Advanced DXTc texture compression and transcoding library"
+    homepage = "https://github.com/FrozenStormInteractive/Crunch"
+    url = "https://github.com/FrozenStormInteractive/Crunch"
+    license = "Zlib"
+    topics = ("conan", "crunch", "texture", "compression", "decompression", "transcoding")
+    settings = "os", "compiler", "arch", "build_type"
+    options = {
+        "fPIC": [True, False],
+        "shared": [True, False],
+    }
+    default_options = {
+        "fPIC": True,
+        "shared": False,
+    }
+    exports_sources = ["CMakeLists.txt", "crnlib/*", "crunch/*", "inc/*", "3rdparty/*"]
+    generators = "cmake"
+
+    _cmake = None
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["CRN_BUILD_EXAMPLES"] = False
+        self._cmake.definitions["CRN_BUILD_SHARED_LIBS"] = self.options.shared
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", src=self.source_folder, dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bindir))
+        self.env_info.PATH.append(bindir)
---
+++ test_package/test_package.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "crnlib.h"
+
+int main()
+{
+    std::cout << "crnlib " << crn_get_version() << std::endl;
+    return 0;
+}
