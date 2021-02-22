8f56454e65bcee9accbde93cc96427e22a4abfa7
Yoann Potinet <yoann.potinet@frozenstorminteractive.com>
2021-02-22 00:24:44 -0500

    Update copyright

--- crunch/crunch.cpp
+++ crunch/crunch.cpp
@@ -5,19 +5,19 @@
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
  * arising from the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  * 1. The origin of this software must not be misrepresented; you must not
  *    claim that you wrote the original software. If you use this software
  *    in a product, an acknowledgment in the product documentation or credits
  *    is required.
- * 
+ *
  * 2. Altered source versions must be plainly marked as such, and must not be
  *    misrepresented as being the original software.
- * 
+ *
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
@@ -1428,7 +1428,7 @@ static void print_title()
     console::printf("crunch - Advanced DXTn Texture Compressor - https://github.com/FrozenStormInteractive/Crunch2");
     console::printf("");
     console::printf("Copyright (c) 2010-2016 Richard Geldreich, Jr. and Binomial LLC");
-    console::printf("Copyright (c) 2020 FrozenStorm Interactive, Yoann Potinet");
+    console::printf("Copyright (c) 2020-2021 FrozenStorm Interactive, Yoann Potinet");
     console::printf("");
     console::printf("crnlib version %s %s Built %s, %s", crn_get_version(), crnlib_is_x64() ? "x64" : "x86", __DATE__, __TIME__);
     console::printf("");
